bd644461a30abd25debd6429027d72e522e102b1
VoidFyre <computernerd552@yahoo.com>
2022-11-02 22:20:05 -0400

    Create ViewTables.sql

---
+++ Scripts/ViewTables.sql
@@ -0,0 +1,11 @@
+USE Project;
+
+SELECT * FROM base;
+SELECT * FROM customer;
+SELECT * FROM customer_order;
+SELECT * FROM discount;
+SELECT * FROM discount_order;
+SELECT * FROM discount_pizza;
+SELECT * FROM pizza;
+SELECT * FROM pizza_topping;
+SELECT * FROM topping;
