f91a33bb456e5fabc3732444ca2911be92fdf14c
Neo0152 <113534226+Neo01521@users.noreply.github.com>
2022-11-29 23:05:57 -0500

    Ha L noah

---
+++ Pizzaria_OS/Scripts/CreateViews.sql
@@ -0,0 +1,68 @@
+-- Written by Neil Kuehn, Christopher Doering
+USE Pizzeria;
+
+DROP VIEW IF EXISTS ToppingPopularity;
+
+CREATE VIEW ToppingPopularity AS
+SELECT
+	topping.ToppingID,
+    COUNT(pizza_topping.ToppingID) AS ToppingCount
+FROM topping, pizza_topping
+WHERE topping.ToppingID = pizza_topping.ToppingID
+GROUP BY pizza_topping.ToppingID
+ORDER BY ToppingCount DESC, topping.ToppingID ASC;
+
+SELECT * FROM ToppingPopularity;
+    
+DROP VIEW IF EXISTS ProfitByPizza;
+
+CREATE VIEW ProfitByPizza AS
+SELECT
+	profit.SizeID AS PizzaSize,
+    profit.CrustID AS PizzaCrust,
+    CAST(TypeProfit AS DECIMAL(6,2)) AS Profit,
+    LastOrderDate
+FROM
+    (SELECT 
+		SizeID,
+        CrustID,
+		ROUND(SUM(PizzaPrice-PizzaCost),2) AS TypeProfit,
+        MAX(PizzaDate) AS LastOrderDate
+	 FROM
+		pizza
+	 WHERE PizzaDate BETWEEN '2022-01-01' AND '2022-12-31'
+	 GROUP BY SizeID,CrustID
+	) AS profit
+GROUP BY SizeID,CrustID
+ORDER BY TypeProfit DESC;
+
+SELECT * FROM ProfitByPizza;
+
+DROP VIEW IF EXISTS ProfitByOrderType;
+
+CREATE VIEW ProfitByOrderType AS
+
+SELECT
+    OrderType AS CustomerType,
+    DATE_FORMAT(OrderTime,'%Y-%M') AS OrderDate,
+    ROUND(SUM(OrderPrice),2) as TotalOrderPrice,
+    ROUND(Sum(OrderCost),2) as TotalOrderCost,
+    ROUND(Sum(OrderPrice-OrderCost),2) as Profit
+FROM
+    customer_order
+GROUP BY 
+    MONTH(OrderTime),
+    OrderType
+UNION all
+select
+    NULL,
+    'Grand Total',
+    round(sum(OrderPrice),2) as TotalOrderPrice,
+    round(sum(OrderCost),2) as TotalOrderCost,
+    round(sum(OrderPrice-OrderCost),2) as Profit
+from
+    customer_order;
+    
+SELECT * FROM ProfitByOrderType;
+
+
---
+++ Pizzaria_OS/Scripts/DropTables.sql
@@ -0,0 +1,12 @@
+-- Written by Neil Kuehn, Christopher Doering
+USE Pizzeria;
+DROP TABLE IF EXISTS pizza_topping;
+DROP TABLE IF EXISTS topping;
+DROP TABLE IF EXISTS discount_order;
+DROP TABLE IF EXISTS discount_pizza;
+DROP TABLE IF EXISTS discount;
+DROP TABLE IF EXISTS pizza;
+DROP TABLE IF EXISTS customer_order;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS base;
+DROP SCHEMA IF EXISTS Pizzeria;
